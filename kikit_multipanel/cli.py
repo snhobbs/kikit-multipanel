f07ab49d5ed18c4e2652bbe460dbe674c5228736
simon <simon.hobbs@electrooptical.net>
2023-11-24 14:25:26 -0500

    adding version to cli Bump version: 0.1.0 → 0.1.1

--- kikit_multipanel/cli.py
+++ kikit_multipanel/cli.py
@@ -1,16 +1,17 @@
 """Console script for kikit_multipanel."""
 import sys
 import click
-from . import kikit_multipanel
+from . import kikit_multipanel, __version__
 from kikit import panelize_ui_impl
 import pandas
 
 
 @click.command()
-@click.option("--fname", required=True)
-@click.option("--out", required=True)
-@click.option("--preset_f", required=True)
-def main(fname, out, preset_f):
+@click.option("--fname", required=True, help="XLS or XLSX panel spreadsheet")
+@click.option("--out", required=True, help="Output filename. Should end in .kicad_pcb")
+@click.option("--preset_f", required=True, help="Kikit preset json file")
+@click.version_option(__version__)
+def main(fname, out, preset_f, verison):
     preset = panelize_ui_impl.loadPreset(preset_f)
     panelize_ui_impl.postProcessPreset(preset)
     df = pandas.read_excel(fname)
